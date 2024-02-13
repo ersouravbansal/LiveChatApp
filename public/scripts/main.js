8c49c78ebec667271f50d1667e636e064a5141bb
Sourav <sourav@192.168.1.2>
2024-02-13 20:58:34 +0530

    added chatrooms

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -12,94 +12,35 @@ $(document).ready(function () {
 
     const selectedChatroom = $(this).data("chatroom");
     if (selectedChatroom) {
-      // Add the 'selected' class to the clicked chatroom block
-      $(this).addClass("selected");
+      const passcode = prompt("Enter the passcode for the room:");
 
-      // Join the selected chatroom
-      socket.emit("join", selectedChatroom);
+      if (passcode) {
+        // Add the 'selected' class to the clicked chatroom block
+        $(this).addClass("selected");
 
-      // Fetch messages for the selected chatroom
-      getMessages(selectedChatroom);
+        // Join the selected chatroom
+        socket.emit("join", { chatroom: selectedChatroom, passcode });
+
+        // Fetch messages for the selected chatroom
+        getMessages(selectedChatroom);
+      }
     }
   });
 
   // Handle creating a new chatroom
   $("#createRoom").click(function () {
     const newRoomName = prompt("Enter the name for the new room:");
-    if (newRoomName) {
-      createChatroom(newRoomName);
+    const newRoomPasscode = prompt("Enter the passcode for the new room:");
+
+    if (newRoomName && newRoomPasscode) {
+      createChatroom(newRoomName, newRoomPasscode);
     }
   });
-  // Initialize the agent at application startup.
-  const fpPromise = import("https://openfpcdn.io/fingerprintjs/v4").then(
-    (FingerprintJS) => FingerprintJS.load()
-  );
 
-  // Get the visitor identifier when you need it.
-  fpPromise
-    .then((fp) => fp.get())
-    .then((result) => {
-      // This is the visitor identifier:
-      visitorId = result.visitorId;
-      console.log("device id:", visitorId);
-    });
+  // ... (rest of your code remains unchanged)
 
-  $("#send").click(function () {
-    sendMessage({
-      name: $("#name").val(),
-      message: $("#message").val(),
-      chatroom: $(".chatroom-block.selected").data("chatroom"),
-      userId: visitorId
-    });
-  });
-
-  socket.on("message", addMessage);
-
-  // Listen for newRoom event
-  socket.on("newRoom", function (newRoomName) {
-    // Add the new room to the list
-    addChatroom(newRoomName);
-  });
-
-  function fetchChatrooms() {
-    $.get("/chatrooms", function (data) {
-      data.forEach((chatroom) => {
-        addChatroom(chatroom);
-      });
-    });
-  }
-
-  function addChatroom(chatroom) {
-    $("#chatroomList").append(`
-      <div class="chatroom-block" data-chatroom="${chatroom}">
-        ${chatroom}
-      </div>
-    `);
-  }
-
-  function addMessage(message) {
-    $("#messages").prepend(`
-      <div class="message">
-        <strong>${message.name}:</strong> ${message.message}
-      </div>
-    `);
-  }
-
-  function getMessages(chatroom) {
-    $.get(`/messages/${chatroom}`, function (data) {
-      $("#messages").empty();
-      data.forEach(addMessage);
-    });
-  }
-
-  function sendMessage(message) {
-    $.post("/messages", message);
-    $("#name").val("");
-    $("#message").val("");
-  }
-
-  function createChatroom(newRoomName) {
-    // Emit an event to the server to create the new chatroom
-    socket.emit("createRoom", newRoomName);
+  function createChatroom(newRoomName, newRoomPasscode) {
+    // Emit an event to the server to create the new chatroom with passcode
+    socket.emit("createRoom", { name: newRoomName, passcode: newRoomPasscode });
   }
 });
